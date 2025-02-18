e070b498417047ae7b232560d8652228c6a0522d
Kalakkal Karthikeyan <kalakkalkarthi6838@gmail.com>
2025-02-18 07:06:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let pipes = [];
     let pipeWidth = 50;
-    let pipeGap = 120;
+    let pipeGap = 160;
     let pipeSpeed = 1.5; // Slower start
     let score = 0;
     let highScore = localStorage.getItem("flappyHighScore") || 0;
